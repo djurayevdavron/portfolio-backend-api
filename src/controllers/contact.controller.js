33f6a406dc5c087026bb1e176ccfb4b8adfecdae
djurayevdavron <djurayevdavron7@gmail.com>
2026-06-27 23:09:12 +0500

    Update contact.controller.js

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -5,19 +5,18 @@ export const sendContactEmail = async (req, res) => {
     const { name, email, subject, message } = req.body;
 
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
+      host: process.env.SMTP_HOST,
+      port: Number(process.env.SMTP_PORT),
       secure: false,
       auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+        user: process.env.SMTP_EMAIL,
+        pass: process.env.SMTP_PASSWORD,
       },
-      requireTLS: true,
     });
 
     await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: process.env.EMAIL_USER,
+      from: process.env.SENDER_EMAIL,
+      to: process.env.SENDER_EMAIL,
       subject: `Portfolio Contact: ${subject}`,
       html: `
         <h2>New Portfolio Message</h2>
